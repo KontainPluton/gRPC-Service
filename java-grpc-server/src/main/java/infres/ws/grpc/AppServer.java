0db28cc534577cb15e54928307469ede91a83de8
Mydalia <77779115+Mydalia@users.noreply.github.com>
2022-01-14 12:20:37 +0100

    Chgt en 8070 pour le server

--- java-grpc-server/src/main/java/infres/ws/grpc/AppServer.java
+++ java-grpc-server/src/main/java/infres/ws/grpc/AppServer.java
@@ -8,7 +8,7 @@ import java.io.IOException;
 public class AppServer {
     public static void main(String[] args) throws IOException, InterruptedException {
 
-        Server server = ServerBuilder.forPort(8080).addService(new BookHotelRoom()).build();
+        Server server = ServerBuilder.forPort(8070).addService(new BookHotelRoom()).build();
 
         server.start();
         server.awaitTermination();
